d765b29145170394ca8a0769153111b25d99df93
Eralandin <eriktt2005teuhreb@mail.ru>
2025-03-16 13:12:10 +0700

    Удалил лишние файлы и отказался от идеи с permissions.py
    Удалил лишнее да отказался от задумки с отдельной обработкой разрешений

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -9,7 +9,6 @@ from .permissions import IsAuthorOrReadOnly
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -26,7 +25,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
@@ -34,7 +32,8 @@ class CommentViewSet(viewsets.ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user, post_id=self.kwargs['post_id'])
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
     
     def perform_destroy(self, instance):
         if instance.author != self.request.user:
--- yatube_api/posts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('api/v1/api-token-auth/', obtain_auth_token),
-]
